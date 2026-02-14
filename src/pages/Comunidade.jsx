3283d7fcbe0221461eb30295a1d7c605ec82f1ac
MKTTeus <smatheussd@gmail.com>
2026-02-14 13:48:39 -0300

    Adição de menu deslizante/Dashboard melhorado

---
+++ src/pages/Comunidade.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Comunidade.css";
+
+function Comunidade() {
+  return (
+    <div>
+      <h1>Cadastro de Comunidade</h1>
+      <p>Aqui ficará o cadastro da comunidade.</p>
+    </div>
+  );
+}
+
+export default Comunidade;
